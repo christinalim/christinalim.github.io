a7d1821a182e65da85f49e2fd781324d3623fabd
Christina Lim <clim@Christinas-MacBook-Pro.local>
2019-01-18 09:19:39 -0500

    Initial commit

---
+++ title.js
@@ -0,0 +1,16 @@
+var i = 0;
+var txt = "Hi, I'm Christina"; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function pageScroll() {
+    window.scrollBy(0,10);
+    scrolldelay = setTimeout(pageScroll,10);
+}
