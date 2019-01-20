7481da8e31d520353bb18986b5b12771d5f0c7e3
Christina Lim <clim@Christinas-MacBook-Pro.local>
2019-01-20 01:39:49 -0500

    project pages, images, reorganization :x

--- title.js
+++ title.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = "Hi, I'm Christina"; /* The text */
+var txt = "Hi, I'm Christina."; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
